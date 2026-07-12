5e98669e935a04be6b925d9caf3b234a8e91c3e9
amrutak826 <amrutakadali@gmail.com>
2026-07-12 16:40:34 +0530

    Delete _Polish/transit_ops directory

--- _Polish/transit_ops/models/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-from . import transit_driver
-from . import transit_vehicle
-from . import transit_trip
-from . import transit_maintenance
-from . import transit_fuel_log
--- _Polish/transit_ops/wizard/__init__.py
+++
@@ -1,2 +0,0 @@
-# -*- coding: utf-8 -*-
-# Wizard modules will be imported here as they are added.
